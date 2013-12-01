05fcd4208e07796c7bdb1c591f4c47e594239e8b
Olivier Dolbeau <contact@odolbeau.fr>
2013-12-01 16:29:33 +0100

    Create basic form

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
 $app['cache.path'] = __DIR__ . '/cache';
-$app['twig.options.cache'] = $app['cache.path'] . '/twig';
+//$app['twig.options.cache'] = $app['cache.path'] . '/twig';
 
 return $app;
