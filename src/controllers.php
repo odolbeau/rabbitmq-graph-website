88778d9f1331e89adb07a7596935d65c0b4aea66
Olivier Dolbeau <contact@odolbeau.fr>
2013-12-02 22:51:44 +0100

    Can now update graph with jquery.graphviz plugin

--- src/controllers.php
+++ src/controllers.php
@@ -3,6 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 use Bab\RabbitMqGraph\Client;
 use Bab\RabbitMqGraph\Graph;
+use Symfony\Component\Process\ProcessBuilder;
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig');
@@ -17,10 +18,25 @@ $app->post('/', function (Request $request) use ($app) {
         return $app->redirect('/');
     }
 
-    $graph = new Graph(uniqid(), $definitions);
+    $graph = new Graph('my_graph', $definitions);
+    $dotContent = $graph->render();
+
+    $tmpFile   = tempnam(sys_get_temp_dir(), 'bbc_');
+    $tmpTarget = tempnam(sys_get_temp_dir(), 'bbc_');
+    file_put_contents($tmpFile, $dotContent);
+
+    $process = ProcessBuilder::create(array('dot', '-Tsvg', '-o'.$tmpTarget, $tmpFile))->getProcess();
+    $process->run();
+    if (!$process->isSuccessful()) {
+        throw new \RuntimeException($process->getErrorOutput());
+    }
+
+    $content = file_get_contents($tmpTarget);
+    unlink($tmpFile);
+    unlink($tmpTarget);
 
     return $app['twig']->render('result.html.twig', array(
-        'graph' => $graph->render(),
+        'graph' => $content,
     ));
 });
 
